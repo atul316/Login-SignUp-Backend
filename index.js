5d9bf6d503045ff80b7b08c8141202ee7f8fd95a
Atul Chowdhury <atulchowdhury316@gmail.com>
2024-05-23 20:08:12 +0530

    added

---
+++ index.js
@@ -0,0 +1,56 @@
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const FormDataModel = require ('./models/FormData');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+console.log(process.env.MONGO_URI)
+
+mongoose.connect(process.env.MONGO_URI);
+
+//INSERT DATA INTO DATABASE (POST) !!!
+
+app.post('/register', (req, res)=>{
+    const {email, password} = req.body;
+    FormDataModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            res.json("Already registered")
+        }
+        else{
+            FormDataModel.create(req.body)
+            .then(log_reg_form => res.json(log_reg_form))
+            .catch(err => res.json(err))
+        }
+    })
+    
+})
+
+//FIND RECORD/DATA !!!
+
+app.post('/login', (req, res)=>{
+    const {email, password} = req.body;
+    FormDataModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            if(user.password === password) {
+                res.json("Success");
+            }
+            else{
+                res.json("Wrong password");
+            }
+        }
+        else{
+            res.json("No records found! ");
+        }
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listining on http://127.0.0.1:${PORT}`);
+});
